413b7856c974873c1903cbea998e8a75dfe605da
Evan Dudla <evan@dud.la>
2016-05-25 22:35:24 -0400

    Add basic test with shallow rendering

--- tests/components/App.js
+++ tests/components/App.js
@@ -1,7 +1,12 @@
+import React from 'react';
 import expect from 'expect.js';
+import { shallow } from 'enzyme';
+
+import App from '../../app/components/App';
 
 describe('<App />', () => {
   it('should render the application', () => {
-     expect(1).to.be(1);
+    const child = <h1>Rendered from ./app/components/App.js!</h1>;
+    expect(shallow(<App />).contains(child)).to.be(true);
   });
 });
