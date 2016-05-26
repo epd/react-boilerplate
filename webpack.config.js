82a1fe3cdd7b667b9e2d6ca4e54931c37af90db8
Evan Dudla <evan@dud.la>
2016-05-25 21:35:43 -0400

    Add Webpack to project

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+
+exports.entry = __dirname + '/app/main.js';
+exports.output = {
+  path: __dirname + '/build',
+  filename: 'app.js'
+};
+
+exports.module = {
+  loaders: [
+    { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' }
+  ]
+};
